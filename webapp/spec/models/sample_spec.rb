9a0cd0afddfafdc38a8de56a0bbd105f7c948965
Louis Matsuda <lcently@gmail.com>
2014-11-17 20:56:35 +0900

    add fail spec

--- webapp/spec/models/sample_spec.rb
+++ webapp/spec/models/sample_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe 'SampleModel' do
-  it { expect(true).to be_truthy }
+  describe 'success case' do
+    it { expect(true).to be_truthy }
+  end
+
+  describe 'fail case' do
+    it { expect(true).to be_falsey }
+  end
 end
 
